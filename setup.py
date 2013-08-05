94647bcabbca26c8e0802217dd1d95e7e1f64af3
Sarah Bird <sarah@bonvaya.com>
2013-08-05 07:48:10 -0700

    Bootstrap css

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-sortable-listview',
-    version='0.2',
+    version='0.21',
     packages=['sortable_listview'],
     include_package_data=True,
     license=LICENSE,
