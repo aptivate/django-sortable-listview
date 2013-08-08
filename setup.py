6a79eb4ab908ad1ae48b69b08c83e81a13065454
Sarah Bird <sarah@bonvaya.com>
2013-08-08 08:53:06 -0700

    Bump version #

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-sortable-listview',
-    version='0.22',
+    version='0.23',
     packages=['sortable_listview'],
     include_package_data=True,
     license=LICENSE,
